d6c6a24b31e1ffbccf94ffe48cf0d7dcfe4f51e2
Melike KILIC <melike180400@gmail.com>
2019-10-13 17:15:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,8 +9,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-add_element_to_end_of_array << "element"
-p add_element_to_end_of_array
+array << "element"
+p array
 end
 
 def add_element_to_start_of_array(array, element)
