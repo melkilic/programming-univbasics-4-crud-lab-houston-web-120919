ca3b4de3b7636b78cd363eb66594658604555769
Melike KILIC <melike180400@gmail.com>
2019-10-13 17:29:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,8 +23,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-array.shift[0]
-p array
+
 end
 
 def retrieve_element_from_index(array, index_number)
@@ -40,5 +39,5 @@ array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+array[index_number] = "element"
 end
